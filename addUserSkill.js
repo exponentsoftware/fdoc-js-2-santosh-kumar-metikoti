d89acfbac935bdb44514478b8dabb26579cbd1af
santosh kumar <santosh.metikoti@gmail.com>
2021-09-20 10:47:27 +0530

    day-2-assignment

---
+++ addUserSkill.js
@@ -0,0 +1,58 @@
+const users = [
+    {
+        name:'Brook', 
+        scores:75,
+        skills:['HTM', 'CSS', 'JS'],
+        age:16
+    },
+    {
+        name:'Alex', 
+        scores:80,
+        skills:['HTM', 'CSS', 'JS'],
+        age:18
+    }, 
+    {
+        name:'David', 
+        scores:75,
+        skills:['HTM', 'CSS'],
+        age:22
+    }, 
+    {
+        name:'John', 
+        scores:85,
+        skills:['HTM'],
+        age:25
+    },
+    {
+        name:'Sara',
+        scores:95,
+        skills:['HTM', 'CSS', 'JS'],
+        age: 26
+    },
+    {
+        name:'Martha', 
+        scores:80,
+        skills:['HTM', 'CSS', 'JS'],
+        age:18
+    },
+    {
+        name:'Thomas',
+        scores:90,
+        skills:['HTM', 'CSS', 'JS'],
+        age:20
+    }
+    ];
+
+    function addUserSkill( name, skill ) {
+        for (var i in users) {
+          if (users[i].name == name) {
+             users[i].skills.push(skill);
+             break;
+          }
+        }
+     }
+
+
+     
+    addUserSkill("John", "react JS");
+    console.table(users)
---
+++ editUser.js
@@ -0,0 +1,58 @@
+const users = [
+    {
+        name:'Brook', 
+        scores:75,
+        skills:['HTM', 'CSS', 'JS'],
+        age:16
+    },
+    {
+        name:'Alex', 
+        scores:80,
+        skills:['HTM', 'CSS', 'JS'],
+        age:18
+    }, 
+    {
+        name:'David', 
+        scores:75,
+        skills:['HTM', 'CSS'],
+        age:22
+    }, 
+    {
+        name:'John', 
+        scores:85,
+        skills:['HTM'],
+        age:25
+    },
+    {
+        name:'Sara',
+        scores:95,
+        skills:['HTM', 'CSS', 'JS'],
+        age: 26
+    },
+    {
+        name:'Martha', 
+        scores:80,
+        skills:['HTM', 'CSS', 'JS'],
+        age:18
+    },
+    {
+        name:'Thomas',
+        scores:90,
+        skills:['HTM', 'CSS', 'JS'],
+        age:20
+    }
+    ];
+
+
+    
+    function editUser( name, newName ) {
+        for (var i in users) {
+          if (users[i].name == name) {
+             users[i].name = newName;
+             break;
+          }
+        }
+     }
+
+    editUser('John','santosh'); //john replaced with santosh
+    console.table(users)
